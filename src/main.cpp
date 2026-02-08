56c40869d42a360d4c277e61dd6d6108d4518cc1
elalabaj <ela@labaj.com.pl>
2026-02-08 20:36:28 +0100

    added Vector2 and Body classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include "../physics/physics.hpp"
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
+	sf::RenderWindow window( sf::VideoMode( { 800, 600 } ), "SFML works!" );
+
+	World world;
+	RigidBody circle{};
+	circle.mass = 10.f;
+	world.bodies.push_back(circle);
+
+	sf::CircleShape shape( 10.f );
 	shape.setFillColor( sf::Color::Green );
 
+	float Time = 0.f;
+	sf::Clock clock;
+	clock.reset();
 	while ( window.isOpen() )
 	{
+		float deltaTime = clock.restart().asSeconds();
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
 				window.close();
 		}
+		
+		world.update(deltaTime);
+		shape.setPosition({world.bodies[0].pos.x, world.bodies[0].pos.y});
 
 		window.clear();
 		window.draw( shape );
