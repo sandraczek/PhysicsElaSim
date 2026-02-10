7e0c2d2613067cfeaab7e765eb80d4cf3bdb7ba7
elalabaj <ela@labaj.com.pl>
2026-02-10 13:10:14 +0100

    added ability to pause with space

--- src/main.cs
+++ src/main.cs
@@ -8,27 +8,56 @@ class App
     static void Main(string[] args)
     {
         RenderWindow window = new(new VideoMode(new Vector2u(800, 600)), "Physics Simulation");
-        window.Closed += (sender, e) => window.Close();
-        Renderer renderer = new();
 
         World world = new(new Vector2(0, 9.81f));
+        Renderer renderer = new();
 
-        RigidBody circleBody = new(
+        RigidBody circle = new(
             new Circle(10f),
             new Vector2(100, 100),
             invMass: 0.1f,
             restitution: 0.7f
         );
-        world.Bodies.Add(circleBody);
+        world.Bodies.Add(circle);
+
+        RigidBody rect = new(
+            new Rectangle(20f, 10f),
+            new Vector2(200, 100),
+            invMass: 0,
+            restitution: 0.2f
+        );
+        world.Bodies.Add(rect);
 
         Clock clock = new();
+        float accumulator = 0f;
+        const float timeStep = 1f / 60f;
+
+        bool isPaused = false;
+
+        window.Closed += (sender, e) => window.Close();
+        window.KeyPressed += (sender, e) =>
+        {
+            if (e.Code == Keyboard.Key.Space)
+                isPaused = !isPaused;
+        };
 
         while (window.IsOpen)
         {
-            float delatTime = clock.Restart().AsSeconds();
-            world.Update(delatTime);
-
             window.DispatchEvents();
+
+            if (!isPaused)
+            {
+                float dt = clock.Restart().AsSeconds();
+                accumulator += dt;
+                while (accumulator >= timeStep)
+                {
+                    world.Update(timeStep);
+                    accumulator -= timeStep;
+                }
+            }
+            else
+                clock.Restart();
+
             window.Clear(new Color(30, 30, 30));
 
             renderer.Render(window, world);
@@ -37,4 +66,3 @@ class App
         }
     }
 }
-
